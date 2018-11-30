083bf02cbedd40731cf7b1d82919b4a213ee89b3
MichaelPC <qq992792890@163.com>
2018-11-30 17:20:37 +0800

    first commit0

---
+++ ex2.py
@@ -0,0 +1,6 @@
+# A comment, this is you can read your program later.
+# Anything after the # is ignored
+print("i could hava code like this.")#the comment after is ignored
+#you can use a comment to disable or comment out code
+#print("this will not run")
+print("this will run.")
---
+++ ex3.py
@@ -0,0 +1,8 @@
+#math and caculate
+print("i will now count my chickens:")
+print("Hens",25+30/6)
+print("Roosters",100-25*3/4)
+print("Now count the eggs")
+print(3+2+1-5+4%2-1/4+6)
+print("Is is true that 3+2<5-7?")
+print(3+2<5-7)
