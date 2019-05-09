61d943c276168e25547f6fe25b93091968d8b2d4
MichaelPC <qq992792890@163.com>
2019-05-09 09:32:42 +0800

    modify ex3.py and add ex4.py

--- ex3.py
+++ ex3.py
@@ -1,8 +1,13 @@
 #math and caculate
 print("i will now count my chickens:")
-print("Hens",25+30/6)
-print("Roosters",100-25*3/4)
+print("Hens",25 + 30 / 6)
+print("Roosters",100 - 25 * 3 / 4)
 print("Now count the eggs")
-print(3+2+1-5+4%2-1/4+6)
+print(3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6)
 print("Is is true that 3+2<5-7?")
-print(3+2<5-7)
+print(3 + 2 < 5 - 7)
+print("what is 3+2?",3 + 2,sep='   ')
+print("what is 5-7?",5 - 7,sep='---|')#use ---| as sep
+print("That is why it is false")
+print("greater?",5 >- 2)
+print("5>=2",5 >= 2)
